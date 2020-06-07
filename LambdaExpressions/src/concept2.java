cbc9ed98ddf9c93587cb15bc9cc26ceebda217f6
Akash Sreeram <akashsreeram18@gmail.com>
2020-06-07 20:47:15 +0530

    Lambdaexpression third commit

--- LambdaExpressions/src/concept2.java
+++ LambdaExpressions/src/concept2.java
@@ -2,13 +2,16 @@ interface Price{
 	double Car(String source, String destination);
 }
 public class concept2 {
-
+	 int instvar = 000; //instance variable
+	static String statvar = "Vroom!!!"; //static variable
 	public static void main(String[] args) {
 		Price p = (source, destination) ->{
-			System.out.print("Your pricr for the folloeing journey is:" + "\n" + source + " " + "to " + destination);
+			String localvar = ";-]";//local variable
+			System.out.println("Your pricr for the folloeing journey is:" + "\n" + source + " " + "to " + destination);
+			System.out.println(" "+statvar+" "+localvar);
 			return 256.5;
 		};
-		double price = p.Car("Kondapur", "BanjaraHills");
+		double price = p.Car("Kondapur", "BanjaraHills");		
 		System.out.print(" "+price);
 	}
 
