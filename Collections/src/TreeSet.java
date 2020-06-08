eebfee69b28d180a217c3f19ad72ca3b1e2e1f23
Akash Sreeram <akashsreeram18@gmail.com>
2020-06-08 18:52:24 +0530

    Tree Commit

--- Collections/src/TreeSet.java
+++ Collections/src/TreeSet.java
@@ -2,18 +2,12 @@ import java.util.*;
 
 public class TreeSet {
 	public static void main(String[] args) {
-
-        TreeSet ts1 = new TreeSet(); 
-  
-        ts1.add("A"); 
-        ts1.add("B"); 
-        ts1.add("C"); 
-  
-        // Duplicates will not get insert 
-        ts1.add("C"); 
-  
-        // Elements get stored in default natural 
-        // Sorting Order(Ascending) 
-        System.out.println(ts1); 
+		TreeSet sta = new TreeSet();
+		sta.
+		sta.add(2);
+		sta.add(1); // has no effect on duplication
+		sta.add(3);
+		System.out.println(sta);
+		System.out.println(sta.contains(3));
 	}
 }
